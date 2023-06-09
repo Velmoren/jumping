82b78fa5f63378143d15440e4a267139995c38b5
Velmoren <velmoren@yahoo.com>
2023-06-09 16:48:57 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const mario = document.getElementById('mario')
-const mushroom = document.getElementById('mushroom')
+const player = document.getElementById('player')
+const damper = document.getElementById('damper')
 const btn = document.getElementById('start-btn')
 
 const events = ['keydown']
@@ -7,29 +7,27 @@ const events = ['keydown']
 let interval
 
 const activeJump = () => {
-    if (!mario.classList.contains('active')) {
-        mario.classList.add('active')
+    if (!player.classList.contains('active')) {
+        player.classList.add('active')
     }
 
     setTimeout(() => {
-        mario.classList.remove('active')
+        player.classList.remove('active')
     }, 300)
 }
 
 const startGame = () => {
-    mushroom.classList.add('animate')
+    damper.classList.add('animate')
 
     events.forEach((event) => {
-        document.addEventListener(event, () => {
-            activeJump()
-        })
+        document.addEventListener(event, activeJump)
     })
 
     interval = setInterval(() => {
-        let marioTop = parseInt(window.getComputedStyle(mario).getPropertyValue('top'))
-        let mushroomLeft = parseInt(window.getComputedStyle(mushroom).getPropertyValue('left'))
+        let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'))
+        let damperLeft = parseInt(window.getComputedStyle(damper).getPropertyValue('left'))
 
-        if (mushroomLeft < 50 && mushroomLeft > 0 && marioTop >= 140) {
+        if (damperLeft < 50 && damperLeft > 0 && playerTop >= 140) {
             endGame()
             alert('GAME OVER!')
         }
@@ -37,8 +35,13 @@ const startGame = () => {
 }
 
 const endGame = () => {
-    mushroom.classList.remove('animate')
+    damper.classList.remove('animate')
+    
     clearInterval(interval)
+
+    events.forEach((event) => {
+        document.removeEventListener(event, activeJump)
+    })
 }
 
 btn.addEventListener('click', () => {
